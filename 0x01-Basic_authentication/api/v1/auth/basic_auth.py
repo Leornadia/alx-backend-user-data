54839024e7f68ad476871a05df981e356c6aecdc
Leornadia <leornadiajose@gmail.com>
2024-09-03 12:44:08 -0700

    basic_auth.py

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -21,10 +21,30 @@ class BasicAuth(Auth):
         """
         if authorization_header is None or not isinstance(authorization_header, str):
             return None
-        
+
         if not authorization_header.startswith("Basic "):
             return None
-        
+
         # Return the part after "Basic " (which is index 6 onward)
         return authorization_header[6:]
+
+    def extract_user_credentials(self, decoded_base64_authorization_header: str) -> (str, str):
+        """
+        Extracts the user email and password from the Base64 decoded value.
+
+        Args:
+            decoded_base64_authorization_header (str): Decoded Base64 authorization header.
+
+        Returns:
+            tuple: The user email and password, or (None, None) if invalid.
+        """
+        if decoded_base64_authorization_header is None or not isinstance(decoded_base64_authorization_header, str):
+            return None, None
+
+        if ':' not in decoded_base64_authorization_header:
+            return None, None
+
+        # Split the string into two parts using the first ':' as the separator
+        email, password = decoded_base64_authorization_header.split(':', 1)
+        return email, password
 
