77e9a1165dca9972af0d92f2de6c487567f9026b
Leornadia <leornadiajose@gmail.com>
2024-09-05 08:43:39 -0700

    engine

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,47 +1,41 @@
 #!/usr/bin/env python3
-""" API module """
+"""
+API V1 Route for Holberton AirBnB project
+"""
 from os import getenv
-from flask import Flask, jsonify
-from models import storage
 from api.v1.views import app_views
+from flask import Flask, jsonify, abort, request
 from flask_cors import CORS
-from api.v1.auth.basic_auth import BasicAuth
-from api.v1.auth.session_auth import SessionAuth  # Import SessionAuth
+from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
-# Choose auth mechanism based on environment variable
 auth = None
 auth_type = getenv('AUTH_TYPE')
-if auth_type == 'basic_auth':
-    auth = BasicAuth()
-elif auth_type == 'session_auth':
-    auth = SessionAuth()  # Assign SessionAuth instance
-
-@app.teardown_appcontext
-def close_db(error):
-    """ Close the database session at the end of the request """
-    storage.close()
+if auth_type == "session_auth":
+    auth = SessionAuth()
+else:
+    auth = Auth()
 
 @app.errorhandler(404)
-def not_found(error):
-    """ 404 Error Handler """
+def not_found(error) -> str:
+    """ Not found handler """
     return jsonify({"error": "Not found"}), 404
 
 @app.errorhandler(401)
-def unauthorized(error):
-    """ 401 Unauthorized Error Handler """
+def unauthorized(error) -> str:
+    """ Unauthorized handler """
     return jsonify({"error": "Unauthorized"}), 401
 
 @app.errorhandler(403)
-def forbidden(error):
-    """ 403 Forbidden Error Handler """
+def forbidden(error) -> str:
+    """ Forbidden handler """
     return jsonify({"error": "Forbidden"}), 403
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
-    port = int(getenv("API_PORT", 5000))
-    app.run(host=host, port=port)
-
+    port = getenv("API_PORT", "5000")
+    app.run(host=host, port=port, debug=True)
