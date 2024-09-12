e7a972876720e95137c67e23a611b6632af3792c
Leornadia <leornadiajose@gmail.com>
2024-09-12 11:26:14 -0700

    auth.py

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,49 +1,56 @@
 #!/usr/bin/env python3
+"""Auth module for user authentication.
 """
-Auth module containing the Auth class and methods.
-"""
 
-from sqlalchemy import create_engine
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy.orm.session import Session
-from user import User, Base
-from sqlalchemy.orm.exc import NoResultFound
+from db import DB
+from user import User  # Make sure to import the User model
+import bcrypt
+from sqlalchemy.exc import NoResultFound
 
-class DB:
-    """DB class to handle database interaction."""
 
-    def __init__(self) -> None:
-        """Initialize a new DB instance."""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
-        Base.metadata.drop_all(self._engine)
-        Base.metadata.create_all(self._engine)
-        self.__session = None
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
 
-    @property
-    def _session(self) -> Session:
-        """Memoized session object."""
-        if self.__session is None:
-            DBSession = sessionmaker(bind=self._engine)
-            self.__session = DBSession()
-        return self.__session
+    def __init__(self):
+        self._db = DB()
 
-    def add_user(self, email: str, hashed_password: str) -> User:
-        """Add a user to the database and return the User object."""
-        new_user = User(email=email, hashed_password=hashed_password)
-        self._session.add(new_user)
-        self._session.commit()
-        return new_user
-
-    def find_user_by(self, **kwargs):
+    def _hash_password(self, password: str) -> str:
         """
-        Find a user by arbitrary keyword arguments.
-        Raise NoResultFound if no such user exists.
-        """
-        query = self._session.query(User).filter_by(**kwargs)
-        user = query.first()
-        if user is None:
-            raise NoResultFound
-        return user
+        Hash a password using bcrypt.
 
+        Args:
+            password (str): The password to hash.
+
+        Returns:
+            str: The hashed password.
+        """
+        # Hash the password using bcrypt and return the hashed value as a string
+        return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt()).decode('utf-8')
+
+    def register_user(self, email: str, password: str) -> User:
+        """
+        Register a new user if it does not exist.
+
+        Args:
+            email (str): The email of the user.
+            password (str): The password of the user.
+
+        Returns:
+            User: The newly created User object.
+
+        Raises:
+            ValueError: If a user with the given email already exists.
+        """
+        try:
+            # Try to find a user with the given email
+            user = self._db.find_user_by(email=email)
+            # If user is found, raise a ValueError
+            raise ValueError(f"User {email} already exists.")
+        except NoResultFound:
+            # If no user is found, proceed with registration
+            hashed_password = self._hash_password(password)
+            # Add the new user to the database
+            new_user = self._db.add_user(email, hashed_password)
+            return new_user
 
